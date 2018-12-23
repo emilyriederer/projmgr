11ce4a2c599d2e75ec175878577163bc1d381cc3
Emily Riederer <emilyriederer@gmail.com>
2018-12-23 11:48:38 -0600

    enhanced post fxs w duplicate check and character return

--- R/post.R
+++ R/post.R
@@ -2,9 +2,13 @@
 #'
 #' @inherit post_engine return params
 #' @param title Issue title (required)
+#' @param distinct Logical value to denote whether issues with the same title
+#'     as a current open issue should be allowed
 #' @export
 #' @family post
 #' @family issues
+#'
+#' @return Character string. "Duplicate" if not posted due to title conflict or "Success" if successfully posted.
 #'
 #' @examples
 #' \dontrun{
@@ -23,7 +27,16 @@
 #'             body = paste( clipr::read_clip(), collapse = "\n") )
 #' }
 
-post_issue <- function(ref, title, ...){
+post_issue <- function(ref, title, ..., distinct = TRUE){
+
+  if(distinct){
+
+    issue_titles <- purrr::map_chr( get_issues(ref, state = 'open') , "title" )
+
+    if(any(title == issue_titles)){ # when title not distinct
+          return("Duplicate")
+      }
+  }
 
   validate_inputs(list(...),
                   allowed_vars = c("body", "milestone",
@@ -34,6 +47,8 @@ post_issue <- function(ref, title, ...){
              title = title,
              ...)
 
+  return("Success")
+
 }
 
 #' Post milestone to GitHub repository
@@ -43,6 +58,8 @@ post_issue <- function(ref, title, ...){
 #' @export
 #' @family post
 #' @family milestones
+#'
+#' @return Character string. "Duplicate" if not posted due to title conflict or "Success" if successfully posted.
 #'
 #' @examples
 #' \dontrun{
@@ -55,6 +72,14 @@ post_issue <- function(ref, title, ...){
 
 post_milestone <- function(ref, title, ...){
 
+  # check for duplicates before attempting to post
+  # github automatically disallows duplicate milestone titles,
+  # so not an optional as in post_issues
+    milestone_titles <- purrr::map_chr( get_milestones(ref, state = 'open') , "title" )
+    if(any(title == milestone_titles)){ # when title not distinct
+      return("Duplicate")
+    }
+
   validate_inputs(list(...),
                   allowed_vars = c("state", "description", "due_on"))
 
@@ -62,5 +87,7 @@ post_milestone <- function(ref, title, ...){
               ref = ref,
               title = title,
               ...)
+
+  return("Success")
 
 }
