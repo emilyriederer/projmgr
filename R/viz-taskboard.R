3860ed56435f60ca89f85fb1488b6596d88c2b1f
Emily Riederer <emilyriederer@gmail.com>
2018-12-27 12:54:50 -0600

    updated design notes

--- R/viz-taskboard.R
+++ R/viz-taskboard.R
@@ -30,34 +30,42 @@ viz_taskboard <- function(issues, str_wrap_width = 20){
 
   data <- issues
 
-  # create dimensions needed for aesthetics ----
+  # create status classification ----
   data$board_group <- "Not Started"
   data$board_group[purrr::map_lgl(data$labels_name, ~"in-progress" %in% .)] <- "In Progress"
   data$board_group[!is.na(data$closed_at)] <- "Done"
   data$board_group <- factor(data$board_group,
                              levels = c("Not Started", "In Progress", "Done"))
 
-  data$board_pos <- -1 * stats::ave(data$number, data$board_group, FUN = seq_along)
+  # create helper aesthetics for size, position, text fmt ----
+  data$board_pos <- stats::ave(data$number, data$board_group, FUN = seq_along)
+  label_components <-
+    purrr::map2(data$number, data$title,
+                ~strwrap(paste0("#", .x, ": ", .y), width = str_wrap_width))
+  data$label <- purrr::map_chr(label_components, ~paste(., collapse = "\n"))
+  #data$height <- max( purrr::map_int(label_components, length) ) * 2
+  height <- max( purrr::map_int(label_components, length) ) * 2
+
 
   # create ggplot object of task board ----
   g <-
-    ggplot(data, aes(x = 1, y = board_pos)) +
-    geom_tile(aes(fill = board_group),
-              width = 0.9, height = 0.9, size = 2) +
-    geom_text(aes(label =
-                    paste0("#", number, ": ", title) %>%
-                    purrr::map(., ~paste(strwrap(., width = str_wrap_width), collapse = "\n"))
-    )) +
-    facet_grid(. ~ board_group, drop = FALSE) +
-    theme(axis.title = element_blank(),
-          axis.text = element_blank(),
-          axis.ticks = element_blank(),
-          panel.grid = element_blank(),
-          strip.text.x = element_text(size = 12),
-          legend.position = 'none')
+    ggplot(data, aes(x = 0, y = 0)) +
+    geom_rect(aes(fill = board_group), xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
+    geom_text(aes(label = label)) +
+    facet_grid(board_pos ~ board_group) +
+    scale_y_continuous(limits = c(round(-1 * height / 1.75), round(height / 1.75))) +
+    theme(
+      panel.grid = element_blank(),
+      axis.title = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      strip.background.y = element_blank(),
+      strip.text.y = element_blank(),
+      #panel.spacing.y = unit(0, "lines"),
+      legend.position = 'none'
+    )
 
-  # add metadata to be used with viz_linked
-
+  # add metadata to be used with viz_linked ----
   class(g) <- c(class(g), "taskboard")
   g[['str_wrap_width']] <- str_wrap_width
 
