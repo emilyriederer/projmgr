5691436e23cc5d978d6b79a33285c8f4326b9393
Emily Riederer <emilyriederer@gmail.com>
2019-01-01 06:07:26 -0600

    enhanced viz gantt and docs for closed val is missing

--- R/viz-gantt.R
+++ R/viz-gantt.R
@@ -1,13 +1,19 @@
-#' Visualize Gannt-style chart of closed issues
+#' Visualize Gannt-style chart of planned or actual time to completion
 #'
-#' Produces plot of all closed issues from start date to end date, based on output of
-#' issues from \code{get_issues()} and \code{parse_issues()}. Plot
-#' has one row per issues spanning from \code{start} to \code{end}.
+#' Produces plot with one vertical bar from the specified \code{start} variable's value to the
+#' \code{end} variable's value. Common uses would be to visualize time-to-completion for issues
+#' gotten by (\code{get_issues} and \code{parse_issues}) or milestones. Bars are colored by
+#' duration with longer bars as a darker shade of blue, and start/completion is denoted by
+#' points at the ends of the bars.
 #'
 #' By default, the start date is the issue's \code{created_at} date, and the end date is
 #' the issue's \code{closed_at} date. However, either of these can be altered via the
 #' \code{start} and \code{end} parameters since these dates might not be reflective of the
 #' true timeframe (e.g. if issues are posted well in advance of work beginning.)
+#'
+#' Unfinished tasks (where the value of the \code{end} variable is \code{NA}) are colored grey and
+#' do not have dots on their bars. Unstarted tasks are dropped because user intent is ambiguous
+#' in that case.
 #'
 #' @param data Dataset, such as those representing issues or milestones (i.e. \code{parse_issues()} or
 #'     \code{parse_milestones()}). Must have unique \code{title} variable and variables  to specify for
@@ -36,23 +42,27 @@ viz_gantt <- function(data, start = created_at, end = closed_at, str_wrap_width 
   # filter data to valid values having start and end
   plot_data <-
     data %>%
-    dplyr::filter(state == "closed", !is.na(!!start_var), !is.na(!!end_var)) %>%
+    dplyr::filter(!is.na(!!start_var)) %>%
     dplyr::arrange(dplyr::desc(!!start_var)) %>%
-    dplyr::mutate(y = factor(title, levels = title))
+    dplyr::mutate(y = factor(title, levels = title),
+                  start_var = !!start_var,
+                  end_var = !!end_var,
+                  psuedo_start_var = dplyr::if_else(is.na(start_var), min(start_var, na.rm = TRUE), start_var),
+                  psuedo_end_var = dplyr::if_else(is.na(end_var), max(end_var, na.rm = TRUE), end_var)
+    )
 
   g <-
-    ggplot(plot_data, aes(
-      x = !!start_var, xend = !!end_var,
-      y = y, yend = y,
-      col = -1*as.integer(difftime(!!end_var, !!start_var, "days"))
-    )) +
+    ggplot(plot_data,
+           aes(x = psuedo_start_var, xend = psuedo_end_var, y = y, yend = y,
+               col = -1*as.integer(difftime(!!end_var, !!start_var, "days"))
+           )) +
     geom_segment(size = 8) +
-    geom_point(aes(x = !!start_var), size = 2) +
-    geom_point(aes(x = !!end_var), size = 2) +
+    geom_point(aes(x = start_var), size = 2) +
+    geom_point(aes(x = end_var), size = 2) +
     labs(title = "Time to Completion") +
     scale_y_discrete(labels = function(x)
       purrr::map(x, ~paste(strwrap(., width = str_wrap_width), collapse = "\n"))
-                     ) +
+    ) +
     theme_bw() +
     theme(axis.text.x = element_text(angle = 45, hjust = 1),
           axis.title.x = element_blank(),
@@ -60,7 +70,6 @@ viz_gantt <- function(data, start = created_at, end = closed_at, str_wrap_width 
           legend.position = "none")
 
   # add metadata to be used with viz_linked
-
   class(g) <- c("gantt", class(g))
   g[['str_wrap_width']] <- str_wrap_width
 
