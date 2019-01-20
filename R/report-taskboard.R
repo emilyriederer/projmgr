7c42887d33a4594fa413ae30c7475c95926c61b5
Emily Riederer <emilyriederer@gmail.com>
2019-01-20 12:12:25 -0600

    implemented html/css based taskboard reporter

--- R/report-taskboard.R
+++ R/report-taskboard.R
@@ -1,101 +1,79 @@
-report_taskboard <- function(data, in_progress_when, include_link = FALSE, hover = FALSE){
+#' Report HTML-based task board of item status
+#'
+#' Produces three column task board showing any relevant objects (typically issues or milestones)
+#' as "Not Started", "In Progress", or "Done".
+#'
+#' The following logic is used to determine the status of each issue:
+#' \itemize{
+#'   \item Done: Items with a \code{state} of "closed"
+#'   \item In Progress: Custom logic via \code{in_progress_when}. See \code{?taskboard_helpers} for details.
+#'   \item Not Started: Default case for items neither In Progress or Closed
+#' }
+#'
+#' @inheritParams viz_taskboard
+#' @param include_link Boolean whether or not to include links back to GitHub
+#' @param hover Boolean whether or not tasks should be animated to slightly enlarge on hover
+#' @param colors Character vector of hex colors for not started, in progress, and complete tasks (respectively)
+#'
+#' @return Returns character string of HTML/CSS with class attribute to be correctly shown "as-is" in RMarkdown
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # in RMarkdown
+#' ```{r}
+#' issues <- get_issues(myrepo, milestone = 1) %>% parse_issues()
+#' report_taskboard(issues, in_progress_when = is_labeled_with('in-progress'))
+#' ````
+#' }
+
+report_taskboard <- function(data,
+                             in_progress_when,
+                             include_link = FALSE,
+                             hover = FALSE,
+                             colors = c("#f0e442", "#56b4e9", "#009e73") ){
 
   stopifnot(is.function(in_progress_when))
+  stopifnot(is.logical(include_link))
+  stopifnot(is.logical(hover))
+  stopifnot(length(colors) == 3)
+  stopifnot(is.character(colors))
+  stopifnot(all(grepl("^#\\w{6}$", colors)))
 
   # create datasets by group ----
   done <- data[data$state == "closed",]
   inprog <- data[in_progress_when(data),]
-  todo <- data[data$state != "closed" & !in_progress_when(data), ]
-
-  # define task generator helper fx ----
-  generate_task_html <- function(class, link, url, title){
-
-    paste(
-      "<div class = 'task", class, "'>",
-      ifelse(link, paste0("<a href = '", url, "' target='_blank'>"), ""),
-      title,
-      ifelse(link, "</a>", ""),
-      "</div>"
-    )
-
-  }
+  notstart <- data[data$state != "closed" & !in_progress_when(data), ]
 
   # html ----
-  done_html <- mapply( function(url, title) generate_task_html("dn", include_link, url, title), done$url, done$title )
-  inprog_html <- mapply( function(url, title) generate_task_html("ip", include_link, url, title), inprog$url, inprog$title )
-  todo_html <- mapply( function(url, title) generate_task_html("ns", include_link, url, title), todo$url, todo$title )
+  done_html <- mapply( function(url, title) fmt_taskboard_item("dn", include_link, url, title), done$url, done$title )
+  inprog_html <- mapply( function(url, title) fmt_taskboard_item("ip", include_link, url, title), inprog$url, inprog$title )
+  notstart_html <- mapply( function(url, title) fmt_taskboard_item("ns", include_link, url, title), notstart$url, notstart$title )
 
-  # add blank divs to balance grid ----
-  n_td <- nrow(todo)
+  # merge columns ----
+  ## note that paste is used over new-lines to enhance readability while ensuring no encoding issues
+  n_ns <- nrow(notstart)
   n_ip <- nrow(inprog)
   n_dn <- nrow(done)
-  n_max <- max(n_td, n_ip, n_dn, na.rm = TRUE)
-  if(n_td < n_max){todo_html[n_td+1:n_max] <- "<div></div>"}
+  n_max <- max(n_ns, n_ip, n_dn, na.rm = TRUE)
+  if(n_ns < n_max){notstart_html[n_ns+1:n_max] <- "<div></div>"}
   if(n_ip < n_max){inprog_html[n_ip+1:n_max] <- "<div></div>"}
   if(n_dn < n_max){done_html[n_dn+1:n_max] <- "<div></div>"}
-  html_tasks <- paste(paste(todo_html, inprog_html, done_html), collapse = "")
-  html <- paste(
-    "<div class = 'taskboard'>
-    <div class = 'head'>To Do</div>
-    <div class = 'head'>In Progress</div>
-    <div class = 'head'>Done</div>",
-    html_tasks,
-    "</div>")
+  html_tasks <- paste(paste(notstart_html, inprog_html, done_html), collapse = "")
+  html <- paste0("<div class = 'taskboard'>",
+                 "<div class = 'head'>Not Started</div>",
+                 "<div class = 'head'>In Progress</div>",
+                 "<div class = 'head'>Done</div>",
+                 html_tasks,
+                 "</div>")
 
-
-  # style ----
-  css <-
-    "
-  .taskboard{
-  display: grid;
-  grid-gap: 1%;
-  grid-template-columns: 1fr 1fr 1fr;
-  margin-bottom: 50px;
-  }
-
-  .head{
-  background-color: #a9a9a9;
-  border: 1px solid #d3d3d3;
-  text-align: center;
-  font-weight: strong;
-  }
-
-  .task{
-  text-align: center;
-  padding: 2%;
-  margin: 2%;
-  border: 1px solid #d3d3d3;
-  box-shadow: 2px 2px 5px grey;
-  transition-duration: 0.5s;
-  }
-
-  .ns{background-color: #f0e442;}
-  .ip{background-color: #56b4e9;}
-  .dn{background-color: #009e73;}
-
-  a, a:visited {
-  color: black;
-  text-decoration: none;
-  }
-
-  a:hover{
-  color: black;
-  text-decoration: underline;}
-
-  a:active{color: white;}
-  "
-
-  css <- paste(
-      "<style>",css,
-      ifelse(hover,
-      " .task:hover{
-      transition-duration: 0.5s;
-      margin: 0%;
-      padding: 4%;}",""),
-      "</style>")
+  # add css ----
+  ## note that paste is used over new-lines to enhance readability while ensuring no encoding issues
+  rand_id <- paste(sample(letters, 10, replace = TRUE), collapse = "")
+  css <- gen_taskboard_css(rand_id, colors, hover)
 
   # aggregate code components ----
-  code <- paste(css, html)
+  code <- paste("<div class = '", rand_id, "'>",css, html, "</div>")
   class(code) <- c("knit_asis", class(code))
   return(code)
 
