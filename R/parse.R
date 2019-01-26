8ed0ed38fc2302213b023f0d2a3d5a4f3f0b1ff6
Emily Riederer <emilyriederer@gmail.com>
2019-01-26 08:35:54 -0600

    fixed date formatting in parse fxs

--- R/parse.R
+++ R/parse.R
@@ -2,7 +2,7 @@
 fmt_safe_chr <- function(x) {ifelse( is.null(x) , NA_character_, x )}
 fmt_safe_int <- function(x) {ifelse( is.null(x) , NA_integer_, x)}
 fmt_safe_lgl <- function(x) {ifelse( is.null(x), NA, x)}
-fmt_safe_date <- function(x) {ifelse( is.null(x), NA, as.Date( substring(x, 1, 10)))}
+fmt_safe_date <- function(x) {as.Date( substring( ifelse( is.null(x), NA, x) , 1, 10)) }
 
 #' Parse issues overview from \code{get_issues}
 #'
@@ -60,6 +60,7 @@ parse_issues <- function( res ) {
                             FUN = function(y)
                               vapply(y[["assignees"]], FUN = function(x) x$login, FUN.VALUE = character(1) ))
 
+  # combine components ----
   data <- do.call(rbind, mapped_elts)
   data$labels_name <- labels_names
   data$assignees_login <- assignees_login
