f10fd48c9a62cf44820130ea48a575ddf8775f73
Emily Riederer <emilyriederer@gmail.com>
2019-01-27 06:09:31 -0600

    added more get tests

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -8,14 +8,14 @@ experigit <- create_repo_ref('emilyriederer', 'experigit')
 
 test_that( "zero item output is returned as type character (vs list)", {
 
-  expect_type( get_engine("/issues", experigit, since = '4000-12-31'), "character")
+  expect_type( projmgr:::get_engine("/issues", experigit, since = '4000-12-31'), "character")
   expect_type( get_issues(experigit, since = '4000-12-31'), "character")
 
 })
 
 test_that( "single item output is wrapped into list of length 1", {
 
-  expect_equal( length(get_engine("/issues/1", experigit)), 1)
+  expect_equal( length(projmgr:::get_engine("/issues/1", experigit)), 1)
   expect_equal( length(get_issues(experigit, 1)), 1)
   expect_equal( length(get_milestones(experigit, number = 10)), 1)
 
@@ -26,3 +26,13 @@ test_that( "get_issues gives message when hitting page limit", {
   expect_message( get_issues(experigit, limit = 1, state = "all"))
 
 })
+
+test_that( "Issue number appended to events / comments only when positive length", {
+
+  expect_true( all(vapply( get_issue_comments(projmgr, 9) , function(x) "number" %in% names(x), logical(1))) )
+  expect_null( names( get_issue_comments(projmgr, 1) ))
+
+})
+
+
+
